a969ab1c17df0f7d1caec20d04dd5e490186ba94
Nhea <serkanyazicioglu@gmail.com>
2019-06-17 14:30:30 +0300

    connstr change

--- TestProject/Repositories/BaseRedisRepository.cs
+++ TestProject/Repositories/BaseRedisRepository.cs
@@ -8,6 +8,6 @@ namespace TestProject.Repositories
     public abstract class BaseRedisRepository<T> : Nhea.Data.Repository.RedisRepository.BaseRedisRepository<T>
          where T : RedisDocument, new()
     {
-        public override string ConnectionString => "testbadconnection:6379,abortConnect=false,connectTimeout=10000,connectRetry=10";
+        public override string ConnectionString => "localhost:6379,abortConnect=false,connectTimeout=10000,connectRetry=10";
     }
 }
