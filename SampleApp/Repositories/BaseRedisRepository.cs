06e52dc82f861f856680d562744c6fabe23c85ae
Nhea <serkanyazicioglu@gmail.com>
2019-06-17 14:01:56 +0300

    redis pipeline changes

--- SampleApp/Repositories/BaseRedisRepository.cs
+++ SampleApp/Repositories/BaseRedisRepository.cs
@@ -8,6 +8,6 @@ namespace SampleApp.Repositories
     public abstract class BaseRedisRepository<T> : Nhea.Data.Repository.RedisRepository.BaseRedisRepository<T>
          where T : RedisDocument, new()
     {
-        public override string ConnectionString => "defaultDatabase=0,<hostname>:<port>,password=<AAAAAAAAAAAAAAAWBBBBBBBBBBBBBBBBBBBB=>,ssl=true,abortConnect=false,connectTimeout=10000,connectRetry=10";
+        public override string ConnectionString => "localhost:6379,abortConnect=false,connectTimeout=10000,connectRetry=10";
     }
 }
